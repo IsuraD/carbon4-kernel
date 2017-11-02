fc19a9ac0bda042d73d6f8aa604ba5c94b0227cc
Prakhash <prakhashsiva21@gmail.com>
2017-11-02 22:45:44 +0530

    formatting done for the space

--- core/org.wso2.carbon.bootstrap/src/main/java/org/wso2/carbon/bootstrap/Bootstrap.java
+++ core/org.wso2.carbon.bootstrap/src/main/java/org/wso2/carbon/bootstrap/Bootstrap.java
@@ -44,7 +44,7 @@ public class Bootstrap {
 	private final Set<URL> classpath = new LinkedHashSet<URL>();
 	private static final String CARBON_HOME = "carbon.home";
 	protected static final String ROOT = System.getProperty(CARBON_HOME, ".");
-    private static final String CARBON_PROPERTIES = "carbon.properties";
+        private static final String CARBON_PROPERTIES = "carbon.properties";
 
     public static void main(String args[]) {
         new Bootstrap().loadClass(args);
